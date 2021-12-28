dadac68078ff9def774d0d6df2bc7633e9cf9611
Indira <indira.km1992@gmail.com>
2021-12-28 07:55:44 +0530

    add lines of code

---
+++ src/org/example/SampleProgram.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class SampleProgram {
+	
+	private void greens() {
+		System.out.println("this is a program");
+
+	}
+	public static void main(String[] args) {
+		SampleProgram si = new SampleProgram();
+		si.greens();
+	}
+
+}
